c1ceb46eba254087f992db70b935019966a90603
Samuel Walladge <samuel@swalladge.id.au>
2016-03-24 13:16:25 +1030

    implemented quiz loading and saving from localstorage (only tested with text inputs currently)

--- js/main.js
+++ js/main.js
@@ -1,38 +1,79 @@
 
+// basic constructor for the Quiz object
+// methods are added to its prototype below
 var Quiz = function() {
-  console.log("quiz initiated!");
   this.canvas = null;
   this.ctx = null;
   this.form = null;
+  this.name = null;
 };
 
+// set the canvas for the quiz to draw to
+// (to be used for displaying scores, feedback, etc.)
 Quiz.prototype.setCanvas = function(canvas, ctx) {
   this.canvas = canvas;
   this.ctx = ctx;
-  console.log("canvas set");
 };
 
+// check the answers!
+// TODO: use the canvas to draw fancy ticks, crosses, visual feedback, etc.
 Quiz.prototype.checkAnswers = function() {
-  // visually display results of quiz - can use canvas to display answers
-  // - draw green ticks, red crosses, etc.
-  this.ctx.fillstyle = "#000000";
+  this.ctx.fillstyle = "#0000AA";
   this.ctx.fillRect(10, 10, 30, 40);
   return false;
 };
 
+// save all answers to localstorage
 Quiz.prototype.saveAnswers = function() {
   // save to local storage
+  var questions = this.form.find('.question');
+  questions.each(function(_, q) {
+    q = $(q);
+    var value = q.val();
+    var question = this.name + '.' + q.prop('name'); // prepend with name for namespacing
+    localStorage.setItem(question, value);
+  });
+
   return false;
 };
 
+
+// load all answers from localstorage, ignores if not set
 Quiz.prototype.loadAnswers = function() {
   // load from local storage if available
+  var questions = this.form.find('.question');
+  questions.each(function(_, q) {
+    q = $(q);
+    var question = this.name + '.' + q.prop('name');
+    var value = localStorage.getItem(question);
+    if (value) {
+      q.val(value);
+    }
+  });
+
+  return false;
+};
+
+// reset all values for the quiz (also resets localstorage)
+Quiz.prototype.reset = function() {
+  var questions = this.form.find('.question');
+  questions.each(function(_, q) {
+    q = $(q);
+    // q.val('');
+    var question = this.name + '.' + q.prop('name'); // prepend with name for namespacing
+    localStorage.removeItem(question);
+  });
+
+  this.form[0].reset();
+
   return false;
 };
 
 
+// initialize the quiz on a form element (by id)
 Quiz.prototype.init = function(formId) {
 
+  this.name = formId;
   this.form = $(formId);
   if (!this.form) {
     console.log("ERROR: no form element found with selector to init quiz");
@@ -46,15 +87,15 @@ Quiz.prototype.init = function(formId) {
   // save and load buttons
   this.form.find('.savequiz').click(this.saveAnswers.bind(this));
   this.form.find('.loadquiz').click(this.loadAnswers.bind(this));
-
+  this.form.find('.resetquiz').click(this.reset.bind(this));
 };
-
 
 
 
 // code to run when page loads
 $(document).ready(function() {
 
+  //TODO: make this non-global when finished testing
   thequiz = new Quiz();
 
   var canvas = document.getElementById('quizcanvas');
@@ -67,6 +108,10 @@ $(document).ready(function() {
   // init the quiz on the element selector
   thequiz.init("#quizform");
   thequiz.setCanvas(canvas, ctx);
+
+  //TODO: auto load from localstorage on start?
+  //TODO: auto save to localstorage on value change?
+  //TODO: test forms with select and radio inputs
 
 });
 
