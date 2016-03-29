b5fb2a2f5000dbce140c27ecb4ce9b8cd2644e68
Samuel Walladge <samuel@swalladge.id.au>
2016-03-29 16:42:31 +1030

    refactoring, error checking, and testing multiply quizzes on a page

--- js/main.js
+++ js/main.js
@@ -1,60 +1,88 @@
 
 /**
+ * @file main.js
  * @project HIT226 Assignment 1
  * @author Samuel Walladge
+ * @copyright Samuel Walladge 2016
  */
 
 /**
  * @class
  * An object to control an interactive quiz. 
  * Can be setup with a form element containing inputs/buttons/etc with appropriate class names.
- * Note: constructor params are optional (if set, will call `init` or `setCanvas` where appropriate.
+ * Note: constructor params are optional (if set, will call `init` or `setCanvas` where appropriate) - can always setup later.
  * @constructor
  * @param {String|undefined} form - if defined, init the quiz on the form
  * @param {String|undefined} canvas - if defined, set the canvas
  */
 var Quiz = function(form, canvas) {
-  this.canvas = null;
-  this.ctx = null;
-  this.form = null;
-  this.name = null;
-  this.questions = null;
+  this.canvas = undefined;
+  this.ctx = undefined;
+  this.form = undefined;
+  this.name = undefined;
+  this.questions = undefined;
   this.numQuestions = 0;
   this.numCorrect = 0;
+  this.qData = {};
+  this.useCanvas = true;
 
   // if form and/or canvas set, init
   if (form) {
-    this.init(form);
-  }
-  if (canvas) {
+    this.init(form, canvas);
+  } else if (canvas) {  // only setcanvas here if no form set, but canvas set
     this.setCanvas(canvas);
   }
 };
 
+Quiz.prototype.noCanvas = function() {
+  this.canvas = undefined;
+  this.ctx = undefined;
+  this.useCanvas = false;
+};
 
 /**
  * set the canvas for the quiz to draw to
  * (for displaying scores, etc. on checking answers
- * @param {String} canvas - the canvas css selector (must return one canvas object)
+ * @param {String|Object} canvas - the canvas css selector (must return one canvas object). Will also work with a dom/jquery object
+ * @return Returns true if a canvas has been set for the quiz (otherwise, false)
  */
 Quiz.prototype.setCanvas = function(canvas) {
-  canvas = $(canvas);
-  if (canvas && canvas.length != 1) {
-    console.log("ERROR: selector returned more than one dom element");
+
+  // attempt to autoset the canvas if 'canvas' not defined
+  if (!canvas) {
+    if (this.form && this.useCanvas) { 
+      canvas = this.form.find('canvas.quizcanvas');
+    } else {
+      return false;
+    }
+  } else {
+    // otherwise, use the 'canvas' selector
+    canvas = $(canvas);
+  }
+
+  if (canvas.length != 1) {
+    console.log("QUIZ ERROR: canvas was nothing or more than one dom element");
     return false; // fail, no canvas
   }
-  // canvas = canvas[0]; // get the raw dom object (using javascript drawing)
+
   if (canvas[0].getContext) { // check if can use canvas element
     this.canvas = canvas; 
     this.ctx = canvas[0].getContext('2d');
   } else {
-    console.log("ERROR: object not a canvas or canvas operations not supported");
+    this.canvas = undefined; 
+    this.ctx = undefined;
+    console.log("QUIZ ERROR: canvas object not a canvas or canvas operations not supported");
+    return false;
   }
+
+  // finally, it worked!
+  this.useCanvas = true;
   return true;
 };
 
 /**
  * check answers (called on user submit quiz)
+ * @return Returns false, thus preventing default button action from happening
  */
 Quiz.prototype.checkAnswers = function() {
   // save the answers first
@@ -104,7 +132,7 @@ Quiz.prototype.checkAnswers = function() {
 };
 
 /**
- * hides the feedback messages for each question
+ * hide the feedback messages for each question
  */
 Quiz.prototype.hideFeedback = function() {
   for (var key in this.qData) {
@@ -113,10 +141,15 @@ Quiz.prototype.hideFeedback = function() {
 };
 
 /**
- * Display the canvas feedback chart
- * @private
+ * display the canvas feedback chart
  */
 Quiz.prototype.displayCanvas = function() {
+
+  // don't try displaying a canvas if no canvas set
+  // also don't error, because a canvas is optional for the quiz
+  if (!this.canvas) {
+    return;
+  }
 
   // hide canvas popup on click
   this.canvas.click(function() {
@@ -184,34 +217,16 @@ Quiz.prototype.displayCanvas = function() {
 };
 
 /**
- * helper function to draw a filled arc on a canvas
- */
-function pizzaSlice(ctx, colour, x, y, radius, start, end) {
-  ctx.beginPath();
-  ctx.lineWidth = "1";
-  ctx.fillStyle = colour;
-  ctx.arc(x, y, radius, start, end);
-  ctx.lineTo(x, y);
-  ctx.fill();
-}
-
-/**
- * helper function to draw a filled circle on a canvas
- */
-function wholePizza(ctx, colour, x, y, radius) {
-  ctx.lineWidth = "3";
-  ctx.fillStyle = colour;
-  ctx.beginPath();
-  ctx.arc(x, y, radius, 0, 2*Math.PI);
-  ctx.fill();
-}
-
-/**
  * save all answers to localstorage. `autosave` uses this method when saving
  * @param {Object|undefined} event - if defined, hides the feedback text from question that trigged the event, 
  *                                   or all feedback if trigged from something else
  */
 Quiz.prototype.saveAnswers = function(e) {
+
+  if (!this.form) {
+    console.log("QUIZ ERROR: Tried to save answers, but I don't have a form element.");
+    return;
+  }
 
   // hide the correct/wrong messages for the current question being changed (if defined),
   //  otherwise hide all
@@ -243,7 +258,7 @@ Quiz.prototype.saveAnswers = function(e) {
     }
   }.bind(this));
 
-  return false;
+  return true;
 };
 
 
@@ -252,6 +267,11 @@ Quiz.prototype.saveAnswers = function(e) {
  * load all answers from localstorage if saved answers available
  */
 Quiz.prototype.loadAnswers = function() {
+
+  if (!this.form) {
+    console.log("QUIZ ERROR: Tried to load answers, but I don't have a form element.");
+    return;
+  }
 
   // answers possibly changed, reset the feedback messages
   this.hideFeedback();
@@ -286,9 +306,14 @@ Quiz.prototype.loadAnswers = function() {
 
 /**
  * reset the quiz - resets form and wipes localstorage for this quiz
+ * @return Returns false, thus preventing default button action from happening
  */
 Quiz.prototype.reset = function() {
-  //TODO: error checking in these functions to make sure form element accessible, etc. before continuing
+
+  if (!this.form) {
+    console.log("QUIZ ERROR: Tried to reset, but I don't have a form element.");
+    return;
+  }
 
   // remove all items for this quiz from localstorage
   var questions = this.form.find('.question');
@@ -313,12 +338,14 @@ Quiz.prototype.reset = function() {
  */
 Quiz.prototype.autosave = function(yes) {
   if (!this.form) {
+    console.log("QUIZ ERROR: Tried to toggle autosave, but I don't have a form element.");
     return;
   }
+
   if (yes) {
     // add event handlers to save answers whenever answer changed
     this.form.find('.question').change(this.saveAnswers.bind(this));
-    this.form.find('.question[type=text]').on('keyup', this.saveAnswers.bind(this));
+    this.form.find('.question[type=text]').on('keydown', this.saveAnswers.bind(this));
   } else {
     // remove event handlers, so will not autosave
     this.form.find('.question').off();
@@ -328,17 +355,17 @@ Quiz.prototype.autosave = function(yes) {
 
 /**
  * initialize the quiz on a form
- * @param {string} formId - the css selector for the form element to use (must return one element!)
+ * @param {String|Object} formId - the css selector (or jquery object) for the form element to use (must return one element!)
+ * @param {String|Object|undefined} canvas - the css selector (or jquery object) for the canvas element to use
  */
-Quiz.prototype.init = function(formId) {
+Quiz.prototype.init = function(formId, canvas) {
 
-  this.name = formId;
   this.form = $(formId);
   if (!this.form) {
-    console.log("ERROR: no form element found with selector to init quiz");
+    console.log("QUIZ ERROR: no form element found with selector to init quiz");
     return false; // fail, no form
   } else if (this.form.length != 1) {
-    console.log("ERROR: selector returned more than one dom element");
+    console.log("QUIZ ERROR: form selector returned more than one dom element");
     return false; // fail, no form
   }
 
@@ -400,9 +427,50 @@ Quiz.prototype.init = function(formId) {
 
   }
 
+  // call setcanvas to attempt to auto-get the canvas
+  this.setCanvas(canvas);
 
   return true;
 };
+
+
+
+/**
+ * helper function to draw a filled arc on a canvas
+ * @param {Object} ctx - canvas 2d context to use
+ * @param {String} colour - canvas 2d context to use
+ * @param {Number} x - x-axis value of arc centre point
+ * @param {Number} y - y-axis value of arc centre point
+ * @param {Number} radius - radius of the arc
+ * @param {Number} start - arc starting angle
+ * @param {Number} end - arc ending angle
+ */
+function pizzaSlice(ctx, colour, x, y, radius, start, end) {
+  ctx.beginPath();
+  ctx.lineWidth = "1";
+  ctx.fillStyle = colour;
+  ctx.arc(x, y, radius, start, end);
+  ctx.lineTo(x, y);
+  ctx.fill();
+}
+
+/**
+ * helper function to draw a filled circle on a canvas
+ * @param {Object} ctx - canvas 2d context to use
+ * @param {String} colour - canvas 2d context to use
+ * @param {Number} x - x-axis value of arc centre point
+ * @param {Number} y - y-axis value of arc centre point
+ * @param {Number} radius - radius of the arc
+ */
+function wholePizza(ctx, colour, x, y, radius) {
+  ctx.lineWidth = "3";
+  ctx.fillStyle = colour;
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, 2*Math.PI);
+  ctx.fill();
+}
+
+
 
 
 
@@ -412,19 +480,33 @@ Quiz.prototype.init = function(formId) {
  */
 $(document).ready(function() {
 
-  //TODO: make this non-global when finished testing
-  thequiz = new Quiz();
+  // // the following is all required to start a quiz (shortest init form)
+  // var thequiz = new Quiz("#quizform");
+  // // autosaving and loadanswers here is optional
+  // thequiz.autosave(true);
+  // thequiz.loadAnswers();
 
-  // init the quiz on the element selector
-  thequiz.init("#quizform");
+  // // alternate, longer form of initializing a quiz
+  // var thequiz = new Quiz();
+  //
+  // // init the quiz on the element selector
+  // thequiz.init("#quizform");
+  //
+  // // specify the canvas to use (or leave blank to auto use available canvas
+  // thequiz.setCanvas();
+  //
+  // // turn on autosave and load saved answers
+  // thequiz.autosave(true);
+  // thequiz.loadAnswers();
 
-  // specify the canvas to use
-  // TODO: allow Quiz to auto-get canvas from first canvas element (with quizcanvas class) in the form element
-  thequiz.setCanvas('#quizform .quizcanvas');
 
-  // turn on autosave and load saved answers
-  thequiz.autosave(true);
-  thequiz.loadAnswers();
+  // I have set the follow class on all quizzes site-wide I want to use
+  //  - also all should autosave and loadAnswers on start to provide a smooth experience
+  $('.my-quizzes').each(function (i, e) {
+    var quiz = new Quiz($(e));
+    quiz.autosave(true);
+    quiz.loadAnswers();
+  });
 
 });
 
