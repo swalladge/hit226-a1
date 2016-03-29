b269b95130e863db656672dfe67450ce9011a44d
Samuel Walladge <samuel@swalladge.id.au>
2016-03-29 21:05:05 +1030

    small tweaks and some fixes for IE

--- README.md
+++ README.md
@@ -5,25 +5,30 @@ Copyright © 2016 Samuel Walladge
 
 ## Assignment 1B Notes
 
-In the works!
-
-### JSDoc api documentation
-
-The Quiz class has [JSDoc](http://usejsdoc.org/) comments - run `jsdoc -d doc js/main.js` from project root directory to generate webpages with docs for using the Quiz class. (to view, open `doc/index.html`)
+The live site for my submission for assignment 1B can be found at https://uni.swalladge.id.au/hit226/assignment1b/.
 
 
 ## Assignment 1A Notes
 
 The live site for my submission for assignment 1A can be found at https://uni.swalladge.id.au/hit226/assignment1a/.
 
+
 ## Source
 
 The full source is managed with git and available online at https://bitbucket.org/swalladge/hit226-a1/src.
+
 
 ## Notes
 
 - The W3C CSS validator throws some errors (`auto is not a flex-flow value : 0 1 auto`) - this can be ignored, since those values in my CSS are valid (source: http://www.w3.org/TR/css-flexbox-1/#valdef-flex-basis-auto).
 - Please don't use Internet Explorer (any versions) to view the website, as it doesn't support flexible boxes layouts properly at all (source: http://caniuse.com/#feat=flexbox).
+- Tested javascript stuff with IE 11 and Edge. Actually works ok! :) (Forget IE 8 though... jQuery 2 doesn't support it, and even if so, half the quiz wouldn't work anyway...)
+
+
+## JSDoc api documentation
+
+The Quiz class has [JSDoc](http://usejsdoc.org/) comments - run `jsdoc -d doc js/main.js` from project root directory to generate webpages with docs for using the Quiz class. (to view, open `doc/index.html`)
+
 
 ## Technical details
 
--- js/main.js
+++ js/main.js
@@ -3,11 +3,24 @@
  * @file main.js
  * @project HIT226 Assignment 1
  * @copyright Samuel Walladge 2016
+ * @license MIT
  */
+
+/*
+ * I am releasing this file, including and limited to the code within (quiz class, helper functions, example jquery to setup quiz), under the terms of the MIT License for the convenience of those who wish to have a quiz on their website somewhere.
+ * Copyright (c) 2016 Samuel Walladge
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
 
 // help for creating classes in javascript found at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript
 // Note: did not use new ecmascript 6 class fanciness, since not widely supported
-
 /**
  * @class
  * An object to control an interactive quiz.
@@ -111,9 +124,7 @@ Quiz.prototype.checkAnswers = function() {
     }
 
     var feedbackStyle = {
-      display: 'block',
-      'background-color': '#FCF2D8',
-      'margin-top': '5px'
+      display: 'block'
     };
     if (correct) {
       this.numCorrect++;
@@ -403,7 +414,13 @@ Quiz.prototype.init = function(formId, canvas) {
     var feedback = $('<div class="quiz-feedback"></div>');
     feedback.css('display', 'none');
     if (q.parent().find('.quiz-feedback').length === 0) {
-      q.parent().prepend(feedback);
+      // must insert after the legend in a fieldset to appease internet explorer...
+      var legend = q.parent().find('legend');
+      if (legend.length === 1) {
+        legend.after(feedback);
+      } else {
+        q.parent().prepend(feedback);
+      }
     }
 
     if (t == 'text') {
